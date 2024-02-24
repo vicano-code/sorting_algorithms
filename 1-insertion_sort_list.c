0838d9dc438486d12f3e18b2c900611df8d04f66
Victor <vc.anokwuru@gmail.com>
2024-02-24 05:16:04 -0800

    make betty compliant

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,43 @@
 #include "sort.h"
 
 /**
+ * swap_head - swap head node and another node
+ * @list: pointer to pointer to list
+ * @current: current node to swap
+ * @tmp: previous node to swap
+ * Return: Nothing
+ */
+void swap_head(listint_t **list, listint_t *current, listint_t *tmp)
+{
+	if (current->next != NULL)
+		current->next->prev = tmp;
+
+	tmp->next = current->next;
+	current->prev = NULL;
+	tmp->prev = current;
+	current->next = tmp;
+	*list = current;
+}
+
+/**
+ * swap_tail - swap tail node and another node
+ * @current: current node to swap
+ * @tmp: previous node to swap
+ * Return: Nothing
+ */
+void swap_tail(listint_t *current, listint_t *tmp)
+{
+	if (current->next == NULL)
+	{
+		tmp->next = NULL;
+		current->prev = tmp->prev;
+		tmp->prev->next = current;
+		tmp->prev = current;
+		current->next = tmp;
+	}
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: pointer to the doubly linked list of integers
@@ -11,7 +48,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current, *tmp;
 	int j = 1;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (&*list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	while (j != 0)
 	{
@@ -23,23 +60,13 @@ void insertion_sort_list(listint_t **list)
 			{
 				tmp = current->prev;
 				if (tmp->prev == NULL)
+					swap_head(list, current, tmp);
+				else if (current->next == NULL)
+					swap_tail(current, tmp);
+				else /*swap between middle nodes*/
 				{
 					current->next->prev = tmp;
 					tmp->next = current->next;
-					current->prev = NULL;
-					tmp->prev = current;
-					current->next = tmp;
-					*list = current;
-				}
-				else
-				{
-					if (current->next == NULL)
-						tmp->next = NULL;
-					else
-					{
-						current->next->prev = tmp;
-						tmp->next = current->next;
-					}
 					current->prev = tmp->prev;
 					tmp->prev->next = current;
 					tmp->prev = current;
